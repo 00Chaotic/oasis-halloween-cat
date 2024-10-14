cc392e7ec5a50a679c9cd4de79a5353cc2eb6bc4
00Chaotic <44113229+00Chaotic@users.noreply.github.com>
2024-10-14 22:25:02 +1100

    Create interactive eyes on black canvas
    This commit adds p5.js and p5-react-wrapper packages to allow for
    image and shape manipulation functionality.
    
    This is then used to create a black canvas with two eyes that follow
    the mouse. On mouse click, an arc is added to make the eyes look angry.
    
    Most unused boilerplate code has been removed.

---
+++ src/Sketch.js
@@ -0,0 +1,74 @@
+import { ReactP5Wrapper } from "@p5-wrapper/react";
+
+/**
+ * Sketch function for p5.js
+ * @param {import("p5")} p5 - The p5 instance
+ */
+function sketch(p5) {
+  let isMousePressed = false;
+
+  // TODO: use for adding background image
+  // p5.preload = () => {
+  //   const bgImg = p5.loadImage();
+  // };
+
+  p5.mousePressed = () => {
+    isMousePressed = true
+  };
+
+  p5.setup = () => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight, p5.WEBGL);
+    p5.angleMode(p5.DEGREES);
+  };
+
+  p5.draw = () => {
+    p5.background(0);
+
+    // Left eye position from center
+    const leftX = -50;
+    const leftY = 0;
+
+    // Calculate angle between left eye and mouse
+    const leftAngle = p5.atan2(p5.mouseY - (p5.windowHeight/2+leftY), p5.mouseX - (p5.windowWidth/2+leftX));
+
+    // Draw left eye
+    p5.push();
+    p5.translate(leftX, leftY);
+    p5.fill(255);
+    p5.ellipse(0, 0, 50, 50);
+    p5.rotate(leftAngle);
+    p5.fill(0);
+    p5.ellipse(12.5, 0, 25, 25);
+    p5.pop();
+
+    // Right eye position from center
+    const rightX = 50;
+    const rightY = 0;
+
+    // Calculate angle between right eye and angle
+    const  rightAngle = p5.atan2(p5.mouseY - (p5.windowHeight/2+rightY), p5.mouseX - (p5.windowWidth/2+rightX));
+
+    // Draw right eye
+    p5.push();
+    p5.translate(rightX, rightY);
+    p5.fill(255);
+    p5.ellipse(0, 0, 50, 50);
+    p5.rotate(rightAngle);
+    p5.fill(0);
+    p5.ellipse(12.5, 0, 25, 25);
+    p5.pop();
+
+    // Add arcs to make eyes look angry when mouse is pressed
+    if (p5.mouseIsPressed) {
+      p5.push();
+      p5.fill(20);
+      p5.arc(-50, 0, 50, 50, 190, 10, p5.OPEN); // Left eye arc
+      p5.arc(50, 0, 50, 50, 170, 350, p5.OPEN); // Right eye arc
+      p5.pop();
+    }
+  };
+}
+
+export default function Sketch() {
+  return <ReactP5Wrapper sketch={sketch} />;
+};

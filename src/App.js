1e52919e9ab545b60bcb4c8cbe32936e3149463a
00Chaotic <44113229+00Chaotic@users.noreply.github.com>
2024-10-18 13:40:13 +1100

    Add touchscreen toast prompting touch for audio
    Due to browser policies, audio can only be played once the user
    has interacted in the form of a click or touch event. On touchscreen
    devices, only a quick tap satisfies this requirement, but holding or
    dragging does not.
    
    For this reason, a brief "toast" has been added to instruct the user
    to tap once to enable audio.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useEffect } from 'react';
 import './App.css';
 import Sketch from './Sketch';
 
 function App() {
+  useEffect(() => {
+    // Return if device is not touchscreen
+    if (!('ontouchstart' in window.document || navigator.maxTouchPoints > 0)) { return; }
+
+    const toast = document.getElementById('toast');
+
+    if (toast) {
+      showToast(toast);
+    }
+  }, []);
+
+  const showToast = (toast) => {
+    toast.classList.remove('hidden');
+    toast.classList.add('show');
+
+    // Automatically hide the toast after 3 seconds
+    setTimeout(() => {
+      toast.classList.remove('show');
+      setTimeout(() => toast.classList.add('hidden'), 500); // Hide AFTER fade-out
+    }, 3000);
+  };
+
   return (
     <div className='App'>
+      <div id='toast' className='toast hidden'>Tap once to enable audio</div>
       <Sketch />
     </div>
   );
